e25229c85813a3f2a420b3bb4f286e9ad070c40b
Later1998 <Lawter.enemy1@gmail.com>
2019-10-19 16:02:47 +0700

    Add Soundtrack

--- src/com/Wave2Game/main/Window.java
+++ src/com/Wave2Game/main/Window.java
@@ -1,33 +1,55 @@
 package com.Wave2Game.main;
 
-
 import java.awt.Canvas;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.io.File;
 
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 public class Window extends Canvas {
 
 	private static final long serialVersionUID = -240840600533728354L;
 
-	public 	Window (int width, int height, String title, Game game){
+	public Window(int width, int height, String title, Game game) {
+
+		File sound = new File("sound.wav");
+
 		JFrame frame = new JFrame(title);
-		
+
 		frame.setPreferredSize(new Dimension(width, height));
 		frame.setMaximumSize(new Dimension(width, height));
 		frame.setMinimumSize(new Dimension(width, height));
-		
+
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setResizable(false);
 		frame.setLocationRelativeTo(null);
 		frame.add(game);
-//		frame.setFocusable(true);
+		frame.setFocusable(true);
 		frame.setVisible(true);
 
-
 		game.start();
+		PlaySound(sound);
 	}
-	
 
+
+	// Sound game
+	static void PlaySound(File Sound) {
+		try {
+			Clip clip = AudioSystem.getClip();
+			clip.open(AudioSystem.getAudioInputStream(Sound));
+			clip.start();
+
+			Thread.sleep(clip.getMicrosecondLength() / 1000);
+		} catch (Exception e) {
+
+		}
+	}
 }
